d425522492e8c131ffb3d30fa382fce4c33740ce
Rob Passante <rob.passante@reurgency.com>
2013-04-05 15:19:21 -0400

    debugging and logging

--- www/js/index.js
+++ www/js/index.js
@@ -30,7 +30,7 @@ var app = {
         document.addEventListener("online", app.onAppIsOnline, false);
         document.addEventListener("offline", app.onAppIsOffline, false);
         document.getElementById("retryConnection_btn").addEventListener("click", this.retryConnectionHandler, false);
-		console.log(document.location.href);
+		
         if (navigator.connection.type == 'none') {
             app.onAppIsOffline();
         } else {
@@ -39,7 +39,7 @@ var app = {
     },
     accessRemoteSite: function(){
         //document.location.href = 'http://www.youngevity.reurgency.com/youngevity_dev1_repapp'; //USE FOR IN BROWSER WITH RIPPLE
-        //window.open('http://www.youngevity.reurgency.com/youngevity_dev1_repapp', '-self', null); //USE FOR ON DEVICE
+        window.open('http://www.youngevity.reurgency.com/youngevity_dev1_repapp', '-self', null); //USE FOR ON DEVICE
     },
     report: function(id) {
         // Report the event in the console
