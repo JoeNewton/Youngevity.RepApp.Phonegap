4fb1f4e0ca19f31ae5412f74d41a6a8ca39e4167
Joe Newton <joe.newton@reurgency.com>
2013-11-21 08:02:30 -0500

    Added Dev2 environment

--- www/js/config.js
+++ www/js/config.js
@@ -2,13 +2,14 @@
  * Environment variable for the remote server. This is used to select the appropriate remote URL
  * @type {string} Environment Name
  */
-const RemoteServerEnvironment = "Test1";
+const RemoteServerEnvironment = "Dev2";
 
 /**
  * Remote URL Variables. One for each possible RemoteServerEnvironment value. Must Be named like
  * RemoteServerEnvironment + "_RemoteURL"
  */
 const Dev_RemoteURL     = "http://www.youngevity.reurgency.com/90forLifeMobile#/";
+const Dev2_RemoteURL     = "http://www.youngevity.reurgency.com/90forLifeMobile#/";
 const Test1_RemoteURL   = "http://test1.youngevity.reurgency.com/90forLifeMobile#/";
 const QA_RemoteURL      = "";
 const Staging_RemoteURL = "http://appstest.youngevity.com/90forLifeMobile/";
